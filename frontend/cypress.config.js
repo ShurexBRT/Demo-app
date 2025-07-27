fec35c501198f1a738609854bd5b5ff8833803ae
ShurexBRT <jovanovicpedja676@gmail.com>
2025-07-28 00:42:58 +0200

    Fix: Clean Cypress config for CI

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,6 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here if needed
-    },
+    supportFile: false,
   },
-});
+};
